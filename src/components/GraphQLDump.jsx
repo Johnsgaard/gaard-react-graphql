bccceb11777438974c9386b400020e807840d63a
axzs <Johnsgaard.J@gmail.com>
2019-03-24 10:22:13 -0600

    Adds more data from github to react component

--- src/components/GraphQLDump.jsx
+++ src/components/GraphQLDump.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import Emoji from 'react-emoji-render';
 
 const GET_USERNAME = gql`
   {
     viewer {
       login
+      avatarUrl
+      status {
+        emoji
+        message
+      }
     }
   }
 `;
@@ -18,7 +24,12 @@ const GraphQLDump = () => {
         if (error) return `Error! ${error.message}`;
 
         return (
-          <div>{data.viewer.login}</div>
+          <React.Fragment>
+            <div>{data.viewer.login}</div>
+            <div>{data.viewer.status.message}</div>
+            <Emoji text={data.viewer.status.emoji} />
+            <img src={data.viewer.avatarUrl} />
+          </React.Fragment>
         );
       }}
     </Query>
