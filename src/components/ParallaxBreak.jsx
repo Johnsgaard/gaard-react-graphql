b57f551a251e2e51aa49173b2f455e7a709e7283
axzs <Johnsgaard.J@gmail.com>
2019-03-26 01:01:59 -0600

    Removes clip path

--- src/components/ParallaxBreak.jsx
+++ src/components/ParallaxBreak.jsx
@@ -5,7 +5,13 @@ const ParallaxBreak = () => (
   <section
     className="small-section bg-dark"
     data-background={GreyFloral}
-    style={{ backgroundImage: `url(${GreyFloral})`, backgroundRepeat: 'repeat', backgroundSize: '200px', clipPath: 'polygon(0px 0px, 100% 0px, 50% 0vw, 0px 6vw)' }}
+    style={{
+      backgroundImage: `url(${GreyFloral})`,
+      backgroundRepeat: 'repeat',
+      backgroundSize: '200px',
+      height: '20px',
+      padding: '0px'
+     }}
   />
 );
 
