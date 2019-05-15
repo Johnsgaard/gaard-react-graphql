7d7de86c5928279c9fadf83ff740e9903ebc2507
Johnsgaards <hello@gaard.ca>
2019-05-15 13:53:56 -0600

    Finishing up dates and test cases

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,18 +7,18 @@ const Error = () => (
   <section className="page-section bg-dark bg-red" data-background="" id="home">
       <div className="relative container">
           <div className="home-content">
-            <div class="home-text">
-              <div class="hs-cont">
-                <div class="hs-wrap">
-                  <div class="hs-line-11 font-alt mb-10">
+            <div className="home-text">
+              <div className="hs-cont">
+                <div className="hs-wrap">
+                  <div className="hs-line-11 font-alt mb-10">
                     Surfing around?
                   </div>
                   <br />
-                  <div class="hs-line-16 no-transp font-alt mb-10">
+                  <div className="hs-line-16 no-transp font-alt mb-10">
                       The page you are looking for may be under construction.
                   </div>
                   <br />
-                  <p class="hs-line-8 no-transp mb-10">
+                  <p className="hs-line-8 no-transp mb-10">
                       Please go back to my <Link to="/">HOME PAGE</Link> to catch some better waves.
                   </p>
                 </div>
