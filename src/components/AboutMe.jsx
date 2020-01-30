1d86f60292c4208bf6d4d91e0c50cc60deb12eca
Joshua Johnsgaard <hello@gaard.ca>
2020-01-29 23:59:22 -0700

    Updates some colours and fonts

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,20 +3,20 @@ import { AgeFromDateString } from 'age-calculator';
 import MyImage from '../images/surf_black-min.png';
 
 const AboutMe = () => (
-  <section className="page-section bg-dark bg-red" data-background="" id="home">
+  <section className="page-section bg-dark bg-beige" data-background="" id="home">
       <div className="relative container">
           <div className="home-content">
               <div className="home-text">
                   <div className="row mt-60 mt-xs-20">
                       
-                      <div className="col-sm-6 col-lg-5 align-center pt-20 pt-lg-0 mb-xs-30">
-                          <div className="hs-line-4 font-alt mb-20">Joshua Johnsgaard</div>
-                          <h1 className="hs-line-15 font-alt mb-40 mb-xs-20">Web Application Developer</h1>
-                          <div className="section-text white mb-70 mb-xs-40 text-justify">
+                      <div className="col-sm-7 col-md-5 col-lg-5 align-center pt-20 pt-lg-0 mb-xs-30">
+                          <div className="hs-line-4 mb-20 text-left">Joshua Johnsgaard</div>
+                          <h1 className="text-justify hs-line-7 mb-40 mb-xs-10">Web Application Developer</h1>
+                          <div className="mb-70 mb-xs-40 text-justify">
                               <p>
                                   <span className="dropcap font-alt">H</span>ello, I'm Joshua. I am a web developer & programmer
-                                   living in Calgary, Alberta Canada. I am passionate about designing and developing web applications and user interfaces. I
-                                   have experience in a diverse set of programming languages and tools, ranging from client side (HTML5, SCSS, Javascript, React, Apollo), 
+                                   living in Calgary, Alberta Canada. I am passionate about designing and developing web applications and user interfaces.
+                                   I have experience in a diverse set of programming languages and tools, ranging from client side (HTML5, SCSS, Javascript, React, Apollo), 
                                    to server side (PHP, Node, C#, Java, GraphQL), all the way to the database (MySQL, Knex). I am always looking for more challenges 
                                    to help me grow as a developer.
                               </p>
@@ -24,9 +24,8 @@ const AboutMe = () => (
                                 Aside from being a web dev, I love to spend my time camping, drawing, playing video games, or 
                                 just hanging out with family & friends.
                               </p>
-                              <hr className="white mb-30" />
-                              <div>
-                                  <h4 className="font-alt mt-0 mb-20">Profile</h4>
+                              <hr className="red" />
+                              <div className="mb-70 mb-xs-40">
                                   <div>
                                       <strong>Age:</strong>
                                       &nbsp;{new AgeFromDateString('1991-11-19').age}
@@ -47,7 +46,7 @@ const AboutMe = () => (
                           </div>
                       </div>
                       
-                      <div className="col-sm-6 col-lg-offset-1">
+                      <div className="col-sm-4 col-md-6 col-sm-offset-1 col-lg-offset-1">
                           <img src={MyImage} className="polaroid" alt=""/>
                       </div>
                       
