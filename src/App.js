443f4b79f690d62af9c3bf99b11d5bb3c52f8534
Johnsgaard <joshua.johnsgaard@ingrooves.com>
2021-08-02 13:29:38 -0700

    feat(ui): :recycle: update class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Apollo
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 // Components
-import SidePanel from './components/SidePanel';
-import MenuButton from './components/MenuButton';
-import Page from './components/Page';
-import MissingPage from './components/MissingPage';
-import DatesToRemember from './components/DatesToRemember';
-import PrivacyPolicy from './components/PrivacyPolicy';
+import SidePanel from "./components/SidePanel";
+import MenuButton from "./components/MenuButton";
+import Page from "./components/Page";
+import MissingPage from "./components/MissingPage";
+import DatesToRemember from "./components/DatesToRemember";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
-// Styles 
-import './template-css/bootstrap.min.css';
-import './template-css/animate.min.css';
-import './template-css/App.scss';
-
-
+// Styles
+import "./template-css/bootstrap.min.css";
+import "./template-css/animate.min.css";
+import "./template-css/App.scss";
 
 // Global.Apollo
 const client = new ApolloClient({
-  uri: 'https://gaard.ca/gaardql',
+  uri: "https://gaard.ca/gaardql",
   cache: new InMemoryCache(),
 });
 
+const App = () => {
+  const [spOpen, setSpOpen] = useState(false);
+  const toggleSidePanel = () => {
+    setSpOpen(!spOpen);
+  };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spOpen: false,
-    };
-  }
-
-  toggleSidePanel = () => {
-    this.setState({ spOpen: !this.state.spOpen });
-  }
-
-  render() {
-    return (
-
-      <Router>
-        <ApolloProvider client={client}>
+  return (
+    <Router>
+      <ApolloProvider client={client}>
         <div className="App">
-          <MenuButton toggleSidePanel={this.toggleSidePanel} />
-          <SidePanel spOpen={this.state.spOpen} toggleSidePanel={this.toggleSidePanel} />
+          <MenuButton toggleSidePanel={toggleSidePanel} />
+          <SidePanel spOpen={spOpen} toggleSidePanel={toggleSidePanel} />
           <Switch>
             <Route path="/" exact component={Page} />
             <Route path="/dates" exact component={DatesToRemember} />
@@ -61,9 +45,7 @@ class App extends Component {
           </Switch>
         </div>
       </ApolloProvider>
-      </Router>
-    );
-  }
-}
-
+    </Router>
+  );
+};
 export default App;
