172715da6173c72f0b0434bb441428895fecc100
Joshua Johnsgaard <hello@gaard.ca>
2024-03-27 15:22:32 -0700

    feat(gaard): :sparkles: allow all origins for apollo-server

--- src/server/index.js
+++ src/server/index.js
@@ -102,7 +102,13 @@ const resolvers = {
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: '*', // Allow requests from any origin. Adjust this based on your requirements.
+  },
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
